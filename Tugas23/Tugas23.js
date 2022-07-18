7df05c617edb9708f25de7556c3b76ca25613dd2
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 20:04:56 +0700

    tugas 23 js
    untuk Niomic

---
+++ Tugas23/Tugas23.js
@@ -0,0 +1,6 @@
+const angka = [32,33,16,40,9,7,11,2,44,1,66]
+
+// membuat array baru dari array angka yang isinya adalah bilangan habis dibagi 2
+const filteredArray = angka.filter(item => item >15);
+
+console.log(filteredArray)
